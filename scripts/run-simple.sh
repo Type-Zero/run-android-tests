e7c3ab1464aaed62c8faa23d179bb8fca461f42c
Roman Kushnarenko <sromku@gmail.com>
2017-03-15 17:08:25 +0200

    Execution scripts

---
+++ scripts/run-simple.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Created by sromku with ☕
+
+planFile=$1
+outputDir=$2
+
+if [ -z $planFile ] || [ -z $outputDir ] ; then
+    echo "Missing params"
+    exit 1
+fi
+
+runningTest="$2/running-test.txt"
+
+for line in `cat "$planFile"`
+do
+
+    # print full test name
+    echo "$line"
+
+    # run test
+    adb shell am instrument -w -e class $line com.sromku.sample.runtests.test/android.support.test.runner.AndroidJUnitRunner > $runningTest
+
+    # SCAN for errors
+    shortReason=''
+    while read l; do
+
+        if [[ $l == "FAILURES"* ]] ;
+        then
+            shortReason=$(head -5 "$runningTest")
+            shortReason=${shortReason//[$'\t\r\n ' / ]}
+            shortReason=${shortReason//[$'\t\r\n']}
+            break
+        fi
+
+    done < "$runningTest"
+
+    # if 'shortReason' is not empty, then we found a bug!
+    if [ ! -z "$shortReason" ] ; then
+
+        echo "[x] FAIL"
+        echo "$shortReason"
+        exit 1
+
+    fi
+
+    echo "[v] OK"
+    echo ""
+
+done
+
+# clean leftovers since we are fine
+if [ -e $runningTest ]; then
+    rm $runningTest
+fi
+
+# looks like we are fine
+echo "All Tests PASSED"
+echo ""
