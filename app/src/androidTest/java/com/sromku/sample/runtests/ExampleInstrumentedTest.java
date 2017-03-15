9fbd989008ecd626ec87b387a9b0c4760fe48b46
Roman Kushnarenko <sromku@gmail.com>
2017-03-15 17:07:45 +0200

    Initial test execution with failing test

--- app/src/androidTest/java/com/sromku/sample/runtests/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sromku/sample/runtests/ExampleInstrumentedTest.java
@@ -4,23 +4,27 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ExampleInstrumentedTest {
+
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.sromku.sample.runtests", appContext.getPackageName());
     }
+
+    @Test
+    public void useAppContextSoundsWrong() throws Exception {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("com.sromku.sample.runtests.wrong", appContext.getPackageName());
+    }
+
 }
