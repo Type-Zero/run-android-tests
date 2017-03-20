a2271f775916c019d1708eddf222da2b9f7202e6
Roman Kushnarenko <sromku@gmail.com>
2017-03-20 13:25:28 +0200

    Test by tags

--- app/src/androidTest/java/com/sromku/sample/runtests/AnnotationsTestPrinter.java
+++ app/src/androidTest/java/com/sromku/sample/runtests/AnnotationsTestPrinter.java
@@ -29,9 +29,17 @@ public class AnnotationsTestPrinter extends InstrumentationRunListener {
         for (Annotation annotation : annotations) {
             if (comm) stringBuilder.append(",");
             stringBuilder.append(annotation.annotationType().getSimpleName());
+
+            // special case of Repeat
             if (annotation instanceof Parameterized.Repeat) {
                 Parameterized.Repeat repeat = (Parameterized.Repeat) annotation;
                 stringBuilder.append(":" + repeat.count());
+            }
+
+            // special case of Tags
+            if (annotation instanceof Tags) {
+                Tags tags = (Tags) annotation;
+                printTags(tags.tags());
             }
             comm = true;
         }
@@ -39,5 +47,18 @@ public class AnnotationsTestPrinter extends InstrumentationRunListener {
         bundle.putString("annotations", stringBuilder.toString());
         getInstrumentation().sendStatus(InstrumentationResultPrinter.REPORT_VALUE_RESULT_START, bundle);
 
+    }
+
+    private void printTags(String[] tags) {
+        Bundle bundle = new Bundle();
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean comm = false;
+        for (String tag : tags) {
+            if (comm) stringBuilder.append(",");
+            stringBuilder.append(tag);
+            comm = true;
+        }
+        bundle.putString("tags", stringBuilder.toString());
+        getInstrumentation().sendStatus(InstrumentationResultPrinter.REPORT_VALUE_RESULT_START, bundle);
     }
 }
---
+++ app/src/androidTest/java/com/sromku/sample/runtests/tags/ExampleInstrumentedTest.java
@@ -0,0 +1,42 @@
+package com.sromku.sample.runtests.tags;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.sromku.sample.runtests.MainActivity;
+import com.sromku.sample.runtests.Tags;
+import com.sromku.sample.runtests.Utils;
+
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public IntentsTestRule<MainActivity> mActivity = new IntentsTestRule<>(MainActivity.class);
+
+    @Test
+    @Tags(tags = {"sanity", "small"})
+    public void useAppContext() throws Exception {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        Utils.sleep(2000);
+        assertEquals("com.sromku.sample.runtests", appContext.getPackageName());
+    }
+
+    @Test
+    @Tags(tags = {"sanity", "medium"})
+    public void useAppContextOther() throws Exception {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        Utils.sleep(4000);
+        assertEquals("com.sromku.sample.runtests", appContext.getPackageName());
+    }
+}
